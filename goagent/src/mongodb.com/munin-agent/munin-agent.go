37ab9808ec8bb5de41ca38bdeb9dabcf27d5398e
David Golub <david.golub@mongodb.com>
2014-07-10 13:16:31 -0400

    Simulate all the Munin data that MMS cares about.

--- goagent/src/mongodb.com/munin-agent/munin-agent.go
+++ goagent/src/mongodb.com/munin-agent/munin-agent.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strings"
 )
 
 func handleConnection(conn *net.TCPConn) {
@@ -14,6 +15,36 @@ func handleConnection(conn *net.TCPConn) {
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		fmt.Printf("%s\n", scanner.Text())
+		tokens := strings.Split(scanner.Text(), " ")
+		switch tokens[0] {
+		case "fetch":
+			switch tokens[1] {
+				case "cpu":
+					conn.Write([]byte("user.value 0\r\n"))
+					conn.Write([]byte("nice.value 0\r\n"))
+					conn.Write([]byte("system.value 0\r\n"))
+					conn.Write([]byte("idle.value 0\r\n"))
+					conn.Write([]byte("iowait.value 0\r\n"))
+					conn.Write([]byte("irq.value 0\r\n"))
+					conn.Write([]byte("softirq.value 0\r\n"))
+					conn.Write([]byte("steal.value 0\r\n"))
+					conn.Write([]byte("guest.value 0\r\n"))
+					conn.Write([]byte(".\r\n"))
+				case "iostat":
+					conn.Write([]byte("dev202_0_read.value 0\r\n"))
+					conn.Write([]byte("dev202_0_write.value 0\r\n"))
+					conn.Write([]byte(".\r\n"))
+				case "iostat_ios":
+					conn.Write([]byte("dev202_1_rtime.value 0\r\n"))
+					conn.Write([]byte("dev202_1_wtime.value 0\r\n"))
+					conn.Write([]byte(".\r\n"))
+			}
+		case "config":
+			conn.Write([]byte("# Unknown service\r\n"))
+			conn.Write([]byte(".\r\n"))
+		case "quit":
+			return
+		}
 	}
 }
 
